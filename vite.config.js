aefdb51460c191949ab7c402e18ce815d39fca6e
shenxiaoliang <shenxiaoliang@192.168.5.228>
2026-06-02 22:29:28 +0800

    fix: set correct base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: './',
+  base: '/image-preview/img/',
   plugins: [vue()],
   server: {
     port: 5173,
